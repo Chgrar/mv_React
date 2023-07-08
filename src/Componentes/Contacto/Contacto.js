55987dffe9ac59c2af07b0771fa50fe5da1f441d
christiangil <ldconesa@gmail.com>
2023-07-08 14:21:04 -0300

    contacto ya terminado

--- src/Componentes/Contacto/Contacto.js
+++ src/Componentes/Contacto/Contacto.js
@@ -1,13 +1,22 @@
 import React from 'react'
+import emailjs from "@emailjs/browser"
 
 export const Contacto = () => {
+
+const sendEmail =(event)=>{
+event.preventDefault()
+emailjs.sendForm("service_52uckah","template_izhkp7b",event.target,"xA2FNPHgbHPo-Lkqa")
+.then(response =>console.log(response))
+.catch(error=>console.log(error))
+}
+
   return (
    
     <div className='div-form'>
       
       <hr/>
         <h2 className='titulo-form'>Contacto</h2>
-        <form className='form-mail'>
+        <form className='form-mail' onSubmit={sendEmail}>
 
             <label>Nombre </label>
             <input type="text" name="user_name"/>
