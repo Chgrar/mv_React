f25071f800fb85b3e5d68838a8347b15ab5ed1f6
christiangil <ldconesa@gmail.com>
2023-07-11 20:39:37 -0300

    banner nuevo

--- src/Componentes/Banner/Banner.js
+++ src/Componentes/Banner/Banner.js
@@ -4,9 +4,21 @@ import "boxicons"
 export const Banner = () => {
   return (
     <div className="Banner">
-        <div className="Banner-container">
-            <h1>MV</h1>
+
+            <h1 className='banner-h1'>MV</h1>
             <p>Indumentaria Deportiva</p>
+
+            <img
+                className="img-banner"
+                src={require(`./../../Imagenes/fotos/ImagenBanner.jpg`)}
+                alt="banner"
+            />
+
+          
+
+        <div className="Banner-container">
+            
+           
             <div className='btn-wsp' >
             <a href='http://api.whatsapp.com/send?phone=1164616606'>
             <box-icon name='whatsapp' type='logo' color='#11ea13' ></box-icon>
--- src/Componentes/Inicio/Inicio.js
+++ src/Componentes/Inicio/Inicio.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import Portada from "../../Imagenes/fotos/ImagenBanner.jpg"
 import { Link } from "react-router-dom"
 
 export const Inicio = () => {
