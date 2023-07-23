359b3357d82cdb80e502b9bfc3cd96ae67db69c0
christiangil <ldconesa@gmail.com>
2023-07-22 21:08:38 -0300

    ultimos cambios 22/7

--- src/Context/Data.js
+++ src/Context/Data.js
@@ -4,19 +4,19 @@ export const Data = {
     {
         id:1,
         nombre:"Remera Slimfit con el escote en V, Blanca",
-        descripcion:"terminaciones finas, hechas de algodon de primera calidad, para acompañarte en tus entrenamientos o salidas",
+        descripcion:"Terminaciones finas, hechas de algodon de primera calidad, para acompañarte en tus entrenamientos o salidas",
         imagen: require("../Imagenes/fotos/Slimfit_V_blanca.jpg"),
         imagen2:require("../Imagenes/fotos/Slimfit_V_blanca.jpg"),
-        precio:5000,
+        precio:5900,
         cantidad:1
     },
     {
         id:2,
         nombre:"Remera Slimfit con el escote en V, Negra",
-        descripcion:"terminaciones finas, hechas de algodon de primera calidad, para acompañarte en tus entrenamientos o salidas",
+        descripcion:"Terminaciones finas, hechas de algodon de primera calidad, para acompañarte en tus entrenamientos o salidas",
         imagen:require("../Imagenes/fotos/Slimfit_V_Negra.jpg"),
         imagen2:require("../Imagenes/fotos/Slimfit_V_Negra.jpg"),
-        precio:5000,
+        precio:5900,
         cantidad:1
     },
     {
@@ -25,7 +25,7 @@ export const Data = {
         descripcion:"Tela de algodón transpirable, super livianas y frescas para usar en cualquier ocasión",
         imagen:require("../Imagenes/fotos/Slimfit_Amplio_Blanca_1.jpg"),
         imagen2:require("../Imagenes/fotos/Slimfit_Amplio_Blanca_2.jpg"),
-        precio:5000,
+        precio:5900,
         cantidad:1
     }, 
     {
@@ -34,7 +34,7 @@ export const Data = {
         descripcion:"Tela de algodón transpirable, super livianas y frescas para usar en cualquier ocasión",
         imagen:require("../Imagenes/fotos/Slimfit_Amplio_Negra_1.jpg"),
         imagen2:require("../Imagenes/fotos/Slimfit_Amplio_Negra_2.jpg"),
-        precio:5000,
+        precio:5900,
         cantidad:1
     },
     {
@@ -43,7 +43,7 @@ export const Data = {
         descripcion:"Tela 100% algodón transpirable que deja respirar la piel y absorbe la humedad del sudor. Frescas, livianas y con todo el estilo",
         imagen:require("../Imagenes/fotos/Musculosa_You_1.jpg"),
         imagen2:require("../Imagenes/fotos/Musculosa_You_2.jpg"),
-        precio:4900,
+        precio:5900,
         cantidad:1
     },
     {
@@ -52,7 +52,7 @@ export const Data = {
         descripcion:"Tela 100% algodón transpirable que deja respirar la piel y absorbe la humedad del sudor. Frescas, livianas y con todo el estilo",
         imagen:require("../Imagenes/fotos/Musculosa_You_3.jpg"),
         imagen2:require("../Imagenes/fotos/Musculosa_You_4.jpg"),
-        precio:4900,
+        precio:5900,
         cantidad:1
     },      
     {
@@ -61,7 +61,7 @@ export const Data = {
         descripcion:"Tela 100% algodón transpirable que deja respirar la piel y absorbe la humedad del sudor. Suaves y super cómodas para entrenar",
         imagen:require("../Imagenes/fotos/Musculosa_Box.jpg"),
         imagen2:require("../Imagenes/fotos/Musculosa_Box.jpg"),
-        precio:4900,
+        precio:5900,
         cantidad:1
     },
     {
@@ -69,66 +69,75 @@ export const Data = {
         nombre:"Short MV Bicolor, deportivo de algodón",
         descripcion:"Holgados y cómodos para tus sesiones de entrenamiento",
         imagen:require("../Imagenes/fotos/Short_Bicolor_Gris.jpg"),
-        imagen2:require("../Imagenes/fotos/Short_Bicolor_Negro.jpg"),
-        precio:5000,
+        imagen2:require("../Imagenes/fotos/Short_Bicolor_Gris.jpg"),
+        precio:5500,
         cantidad:1
     },
     {
         id:9,
-        nombre:"Conjunto deportivo de Lycra",
+        nombre:"Conjunto deportivo Gymsis II, Negro",
         descripcion:"Se adapta a tu cuerpo, cómodo y fresco para entrenar al máximo nivel",
         imagen:require("../Imagenes/fotos/Conjunto_Gymsis2_1.jpg"),
         imagen2:require("../Imagenes/fotos/Conjunto_Gymsis2_2.jpg"),
-        precio:7500,
+        precio:12000,
         cantidad:1
     },
     {
         id:10,
-        nombre:"Conjunto deportivo Gymsis",
+        nombre:"Conjunto deportivo Gymsis, Azul",
         descripcion:" Calzas cortas Tiro medio  //  Top corto espalda abierta",
         imagen:require("../Imagenes/fotos/Conjunto_Gymsis1_3.jpg"),
-        imagen2:require("../Imagenes/fotos/Conjunto_Gymsis1_4.jpg"),
-        precio:7500,
+        imagen2:require("../Imagenes/fotos/Conjunto_Gymsis1_3.jpg"),
+        precio:12000,
         cantidad:1
     },
     {
         id:11,
-        nombre:"Conjunto deportivo Gymsis II",
-        descripcion:"Biker cortas tiro alto  // Remera corta al cuerpo",
-        imagen:require("../Imagenes/fotos/Conjunto_Gymsis2_1.jpg"),
-        imagen2:require("../Imagenes/fotos/Conjunto_Gymsis2_2.jpg"),
-        precio:7500,
+        nombre:"Conjunto deportivo Gymsis, Negro",
+        descripcion:" Calzas cortas Tiro medio  //  Top corto espalda abierta",
+        imagen:require("../Imagenes/fotos/Conjunto_Gymsis1_4.jpg"),
+        imagen2:require("../Imagenes/fotos/Conjunto_Gymsis1_4.jpg"),
+        precio:12000,
         cantidad:1
     },
     {
         id:12,
-        nombre:"Musculosa rústica, Negra",
-        descripcion:"100% algodón transpirable de primera calidad, cómodas y frescas para entrenar al máximo nivel",
-        imagen:require("../Imagenes/fotos/Musculosa_Rustica_Negra.jpg"),
-        imagen2:require("../Imagenes/fotos/Musculosa_Rustica_Negra.jpg"),
-        precio:5000,
+        nombre:"Conjunto deportivo Gymsis II, Azul",
+        descripcion:"Biker cortas tiro alto  // Remera corta al cuerpo",
+        imagen:require("../Imagenes/fotos/Conjunto_Gymsis2_1.jpg"),
+        imagen2:require("../Imagenes/fotos/Conjunto_Gymsis2_2.jpg"),
+        precio:12000,
         cantidad:1
     },
     {
         id:13,
-        nombre:"Musculosa rústica, Blanca",
+        nombre:"Musculosa rústica, Negra",
         descripcion:"100% algodón transpirable de primera calidad, cómodas y frescas para entrenar al máximo nivel",
-        imagen:require("../Imagenes/fotos/Musculosa_Rustica_Blanca.jpg"),
-        imagen2:require("../Imagenes/fotos/Musculosa_Rustica_Blanca.jpg"),
-        precio:5000,
+        imagen:require("../Imagenes/fotos/Musculosa_Rustica_Negra.jpg"),
+        imagen2:require("../Imagenes/fotos/Musculosa_Rustica_Negra.jpg"),
+        precio:5900,
         cantidad:1
     },
     {
         id:14,
-        nombre:"Remeron oversize Minimal",
-        descripcion:"100% algodón transpirable de primera calidad, ideales y comodos para entrenar y para salir",
-        imagen:require("../Imagenes/fotos/Remeron1.jpg"),
-        imagen2:require("../Imagenes/fotos/Remeron2.jpg"),
-        precio:4900,
+        nombre:"Musculosa rústica, Blanca",
+        descripcion:"100% algodón transpirable de primera calidad, cómodas y frescas para entrenar al máximo nivel",
+        imagen:require("../Imagenes/fotos/Musculosa_Rustica_Blanca.jpg"),
+        imagen2:require("../Imagenes/fotos/Musculosa_Rustica_Blanca.jpg"),
+        precio:5900,
         cantidad:1
     },
     {
         id:15,
+        nombre:"Remeron oversize Minimal",
+        descripcion:"100% algodón transpirable de primera calidad, ideales y comodos para entrenar y para salir",
+        imagen:require("../Imagenes/fotos/Remeron1.jpg"),
+        imagen2:require("../Imagenes/fotos/Remeron2.jpg"),
+        precio:5900,
+        cantidad:1
+    },
+    {
+        id:16,
         nombre:"Musculosa minimal urbana, Negra",
         descripcion:"Tela 100% algodón transpirable que deja respirar la piel y absorbe la humedad del sudor. Suaves y super cómodas para entrenar",
         imagen:require("../Imagenes/fotos/Muscu_Minimal_1.jpg"),
@@ -137,7 +146,7 @@ export const Data = {
         cantidad:1
     },
     {
-        id:16,
+        id:17,
         nombre:"Musculosa minimal urbana, Roja",
         descripcion:"Tela 100% algodón transpirable que deja respirar la piel y absorbe la humedad del sudor. Suaves y super cómodas para entrenar",
         imagen:require("../Imagenes/fotos/Muscu_Minimal_3.jpg"),
@@ -146,7 +155,7 @@ export const Data = {
         cantidad:1
     },
     {
-        id:17,
+        id:18,
         nombre:"Remeron VALHALLA oversize, Negro",
         descripcion:"100% algodón transpirable de primera calidad, ideales para entrenar y para salir",
         imagen:require("../Imagenes/fotos/Valhalla_4.jpg"),
@@ -155,7 +164,7 @@ export const Data = {
         cantidad:1
     },
     {
-        id:18,
+        id:19,
         nombre:"Remeron VALHALLA oversize, Rojo",
         descripcion:"100% algodón transpirable de primera calidad, ideales para entrenar y para salir",
         imagen:require("../Imagenes/fotos/Valhalla_3.jpg"),
